0af86ec19392fdc345bac757350c96a18423f708
Andrii Shevchuk <and.sheva2017@gmail.com>
2020-09-16 12:32:15 +0300

    Initial Commit

---
+++ testLampa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testLampa
+//
+//  Created by Андрей Шевчук on 16.09.2020.
+//  Copyright © 2020 Андрей Шевчук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
